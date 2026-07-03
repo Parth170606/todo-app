37518aab4e8cca8455e1fd0eb5cdafb02a0083a3
Parth Nikam <nikamparth06@gmail.com>
2026-07-03 19:03:06 +0530

    git commit -m "feat: prepare application for deployment"

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(cors({
     origin: [
         "http://localhost:5500",
         "http://127.0.0.1:5500",
-        "https://your-vercel-app.vercel.app"
+        "https://todo-bxdykfsor-web-applications1.vercel.app"
     ],
     credentials: true
 }));
